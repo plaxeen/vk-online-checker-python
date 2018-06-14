0d5fddc80a3982f4d1e7d248dcbdd51412a2a5b1
Oleg Plaxin <plaxoleg@gmail.com>
2018-06-15 02:11:50 +0300

    added multi-user support, program restart on exception occurrence, optimization and error correction

--- main.py
+++ main.py
@@ -11,30 +11,42 @@ import time
 import os
 
 
-def check_user(id):
+def check_user(ids):
     TAG = "user checker"
     vk_api_link = "https://api.vk.com/method/"
     access_token = "2cbc788c2cbc788c2cbc788c082cde2a0322cbc2cbc788c7670e35f9811d37cde017f2b"
-    v = "5.74"
-    get_link = vk_api_link + "users.get?user_ids=" + str(
-        id) + "&fields=sex,online,last_seen&access_token=" + access_token + "&v=" + v
+    v = "5.78"
+    user_ids = ",".join(str(e) for e in ids)
+    get_link = vk_api_link + "users.get?user_ids=" + str(user_ids) + "&fields=sex,online,last_seen&access_token=" + access_token + "&v=" + v
 
     ms = int(round(time.time() * 1000))
     content = HTMLSession().get(get_link)
     json = content.json()
     countMillis = int(round(time.time() * 1000)) - ms
 
-    userinfo = json["response"][0]
-    userstat = userinfo["last_seen"]
-
-    nameofuser = userinfo["first_name"] + " " + userinfo["last_name"]
     sex = [["была в сети", "был в сети"], ["в сети", "в сети"]]
     device = ["", "с мобильного", "с iPhone", "", "с Android", "", "с Windows", "с ПК"]
-    ms_time = int(userstat["time"]) + 10800
 
-    log("i", nameofuser + " " + sex[int(userinfo["online"])][int(userinfo["sex"]) - 1] +
-        " " + device[int(userstat["platform"])] + ", " + time.strftime("%d %b %Y %H:%M:%S", time.gmtime(ms_time)))
+    log("br", "")
+    for i in range(num_of_user):
+        userinfo = json["response"][i]
+        userstat = userinfo["last_seen"]
+
+        nameofuser = userinfo["first_name"] + " " + userinfo["last_name"]
+        ms_time = time.gmtime(int(userstat["time"]) + 10800)
+
+        log("online", nameofuser + " " + sex[int(userinfo["online"])][int(userinfo["sex"]) - 1] +
+            " " + device[int(userstat["platform"])] + ", " + time.strftime("%d %b %Y %H:%M:%S", ms_time))
+        title_ch(nameofuser + " " + sex[int(userinfo["online"])][int(userinfo["sex"]) - 1] +
+                 " " + device[int(userstat["platform"])] + ", " + time.strftime("%H:%M:%S", ms_time))
+    log("br", "")
+
     log(TAG, "Задержка " + str(countMillis) + " мс.")
+
+
+def title_ch(title_string):
+    from os import system
+    system("title " + title_string)
 
 
 def log(tag, message):
@@ -44,6 +56,8 @@ def log(tag, message):
     nowtime = time.strftime("%d %b %Y %H:%M:%S", now)
     if str(tag) == "i":
         text_return = "\n" + str(nowtime) + ": " + str(message) + "\n"
+    elif str(tag) == "br":
+        text_return = ""
     else:
         text_return = str(nowtime) + ": " + str(tag) + ": " + str(message)
     print(text_return)
@@ -60,25 +74,35 @@ if __name__ == "__main__":
 
     print("\tVK Online checker\t")
 
-    user = None
-    timer_delay = 1
+    num_of_user = int(input("Колличество отслеживаемых пользователей: "))
+    users = []
+    for i in range(num_of_user):
+        users.append(input("ID пользователя " + str(i+1) + ": "))
+    users.sort()
+
+    tdelay = 1
     if len(sys.argv) == 2:
-        user = sys.argv[1]
-    elif len(sys.argv) == 3:
-        timer_delay = sys.argv[2]
+        tdelay = sys.argv[1]
     else:
-        user = input("Введите ID пользователя: ")
-        timer_delay = input("Введите задержку таймера (мин): ")
+        tdelay = input("Введите задержку таймера (мин): ")
+
+    timer_delay = tdelay
 
     now = time.localtime()
     if now.tm_mon < 10:
         month = "0" + str(now.tm_mon)
     else:
         month = str(now.tm_mon)
-    path = "logs\\" + str(user) + "\\" + str(now.tm_year) + month + "\\"
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+        path = None
+        if len(users) >= 2:
+            users_s = "; ".join(str(e) for e in users)
+            path = "logs\\" + "few users\\" + users_s + "\\" + str(now.tm_year) + month + "\\"
+        else:
+            path = "logs\\" + str(users) + "\\" + str(now.tm_year) + month + "\\"
+
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     while True:
         runtimes += 1
@@ -86,10 +110,16 @@ if __name__ == "__main__":
         log(TAG, "### ###")
         log(TAG, "Программный цикл: " + str(runtimes))
 
-        if user is None and user == "":
-            break
-        else:
-            check_user(user)
+        try:
+            if users is None and users == "":
+                break
+            else:
+                check_user(users)
+            timer_delay = tdelay
+        except Exception as e:
+            log("error", "An error has occurred: " + str(e))
+        finally:
+            timer_delay = 1
 
         uptime = round(time.time() * 1000) - round(startup * 1000)
 
